a8d1240cbd12334b9f9ea5fc7a7862be32b02e3f
Manav Parmar <parmar.manav2000@gmail.com>
2020-09-02 18:49:11 +0530

    navbar completed

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFAcjUCVKWLAnT-TtNVsrAOz3lylYWjZc",
+    authDomain: "facebook-clone-17e32.firebaseapp.com",
+    databaseURL: "https://facebook-clone-17e32.firebaseio.com",
+    projectId: "facebook-clone-17e32",
+    storageBucket: "facebook-clone-17e32.appspot.com",
+    messagingSenderId: "1069423556106",
+    appId: "1:1069423556106:web:46e25f1610a409113c51f3",
+    measurementId: "G-VY4TQMD5K1"
+};
